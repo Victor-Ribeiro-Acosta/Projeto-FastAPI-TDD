5c8d71936353cdfe125c69a04a34378a4d07231e
Victor-Ribeiro-Acosta <victoracosta275@gmail.com>
2025-02-15 22:07:18 -0300

    removendo uri de database

--- src/core/config.py
+++ src/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     PROJECT_NAME: str = 'Cadastro de produtos'
     ROOT_PATH: str = "/"
-    DATABASE_URL: str = "mongodb+srv://victor:mdb2024@projetodio.xhba37y.mongodb.net/ProdutosDB?retryWrites=true&w=majority&appName=ProjetoDIO"
+    DATABASE_URL: str
     model_config = SettingsConfigDict(env_file=".env")
 
 
